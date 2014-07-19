fddb8d5eec21ed9db2d815c1f53f2f9b0bb8e4dd
thang2410199 <thang2410199@gmail.com>
2014-07-19 18:18:00 +0300

    Starter project from azure

---
+++ GGalaxy/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using GGalaxy.Resources;
+
+namespace GGalaxy
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
